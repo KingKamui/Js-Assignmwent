4d3d306072c3b445dbcc784c934119f04dfc5765
Kamui <30941329+KingKamui@users.noreply.github.com>
2017-11-13 11:06:18 -1000

    JS working?

--- exercises.js
+++ exercises.js
@@ -1,34 +1,27 @@
-
 /*
- # Js-Assignmwent
+Js-Assignmwent
 Assignment for C= (F32) / 1.8
-
 Create to a conversion table using the following format:
     C= (F32) / 1.8;
 Start with Fahrenheit temperature of 20 degrees and end with a temperature of 120 degrees; 
 use an increment value of 5.  
 The table will have two columns, one for Fahrenheit and one for those same temperatures converted to Celsius.
-
 Num1 = 20
 Num2 = 32
 Num3 = 1.8
 */
 
-function toCelcius(Fahreheit) {
-    convert(Fahreheit);
-    return(Celcius);
+function toCelsius(Fahrenheit) {
+    convert(Celsius);
+    return(Fahrenheit);
 }
 console.log(toCelcius);
-
-var intTemp = 20
-var maxTemp = 120
-var increm. = 5
 
 var num1 = 20
 var num2 = 32
 var num3 = 1.8
 
-function toCelcius (num1, num2, num3){
+function toCelsius (num1, num2, num3){
      console.log((num1*num2)/num3);
 }
 
@@ -37,4 +30,4 @@ function toCelcius (num1, num2, num3){
 function toCelsius(Fahrenheit){
     return (20*32)/1.8;
 }
-console.log(Fahrenheit);
+console.log(toCelsius);
